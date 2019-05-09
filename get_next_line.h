6ae693afd4c0e30986be1bacd29b1d9450acad69
Karim Khabouri <kkhabour@e2r8p2.1337.ma>
2019-05-09 22:50:13 +0000

    #10

--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,14 @@
 /*   By: kkhabour <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/06 20:25:31 by kkhabour          #+#    #+#             */
-/*   Updated: 2019/05/09 22:19:41 by kkhabour         ###   ########.fr       */
+/*   Updated: 2019/05/09 22:42:36 by kkhabour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include <fcntl.h>
-# include <stdlib.h>
-# include "../libft/libft.h"
-# define BUFF_SIZE 90
+# include "./libft/libft.h"
+# define BUFF_SIZE 32
 
 int						get_next_line(int const fd, char **line);
 typedef struct			s_linkedlist
